18cdc49bf030069c1bda7f548fcb5fcf54c17bc7
Manuj Sharma <technomanuj@hotmail.com>
2022-02-02 17:51:50 +0530

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LearnAppService01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
